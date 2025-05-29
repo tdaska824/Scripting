e99e200fd872b7de7f14ff1fed3ffa239f18e9ad
tdaska824 <theopdask@gmail.com>
2025-05-29 17:07:11 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld: MonoBehaviour
+{
+    void Start()
+    {
+        print("Hello, World!");
+    }
+    void Update()
+    {
+
+    }
+    
+
+
+}
